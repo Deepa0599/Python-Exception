7bda9dd5e2aa44040d4f81d6fc8e8561eaf07780
Deepa0599 <gadhavedeepa07@gmail.com>
2022-04-01 00:28:57 +0530

    Exception

---
+++ main.py
@@ -0,0 +1,20 @@
+
+
+try:
+    age = 18
+    if age < 18:
+        raise ValueError("Not eligible for vote")
+    else:
+        print("Eligible for vote")
+
+except ZeroDivisionError as e:
+    print("Exception occured = >" +str(e) )
+except ValueError as e:
+    print("Exception occured = >" +str(e) )
+except:
+    print("Exception occured")
+else:
+    print("Program executed successfully....")
+finally:
+    print("Harman Ltd")
+
